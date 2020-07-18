d7c70a7a88fc212ff2231081b1bff53b42ab7af2
Jean Villete <jean.villete@gmail.com>
2020-07-18 13:23:44 -0300

    - Adicionando comando de instalação das dependências (principalment do   React) dos projetos FE no script build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,13 @@
 
 # build FE
 pushd fiap.stock.mgnt.front/
+  npm i
   npm run build
   docker build -t fiap.stock.mgnt.front .
 popd
 
 pushd fiap.stock.portal.front/
+  npm i
   npm run build
   docker build -t fiap.stock.portal.front .
 popd
