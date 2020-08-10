a2cd89418f9ff5f2cda7bb5aaad8b3587f00a4f3
Jean Villete <jean.villete@gmail.com>
2020-08-10 20:20:53 -0300

    Adição de markdown para disciplina DEVOPS

--- fiap.stock.mgnt/src/main/java/fiap/stock/mgnt/order/application/OrderController.java
+++ fiap.stock.mgnt/src/main/java/fiap/stock/mgnt/order/application/OrderController.java
@@ -1,11 +1,9 @@
 package fiap.stock.mgnt.order.application;
 
 import fiap.stock.mgnt.common.exception.InvalidSuppliedDataException;
-import fiap.stock.mgnt.order.domain.exception.OrderConflictException;
 import fiap.stock.mgnt.order.domain.exception.OrderIsNotWaitingForResponseException;
 import fiap.stock.mgnt.order.domain.exception.OrderNotFoundException;
 import fiap.stock.mgnt.order.domain.usecase.OrderUseCase;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,16 +16,6 @@ public class OrderController {
 
     public OrderController(OrderUseCase orderUseCase) {
         this.orderUseCase = orderUseCase;
-    }
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public OrderUseCase.OrderPayload insertNewOrder(
-            @PathVariable("loginId") String loginId,
-            @RequestBody OrderUseCase.OrderPayload orderPayload)
-            throws OrderConflictException, InvalidSuppliedDataException {
-
-        return orderUseCase.insertNewOrder(loginId, orderPayload);
     }
 
     @GetMapping
